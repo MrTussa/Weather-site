03f361cbcca0b665906304c726f18222146cd761
MrTussa <dd993736@gmail.com>
2023-02-24 21:55:16 +0500

    modified:   script.js

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-const city = "London"
+const city = "London";
 
 let weather = {
-    "apiKey": "dba7d7b658e0ea9699b67a8ae8aabb6a",
-    weatherFetch: function (city) {
-        fetch(
-            // "http://api.openweathermap.org/geo/1.0/direct?q="
-            // + city + "&limit=5&appid="
-            // + this.apiKey
-            "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=dba7d7b658e0ea9699b67a8ae8aabb6a"
-        )
-            .then(function (response) {
-                return response.json()
-            })
-            .then(function (data) {
-                console.log('data', data[0].lat)
-            })
-            .catch(function (error) {
-                console.log('error', error)
-            })
-        const { lat  = data
-        const { lon } = data.lon
-        fetch(
-            "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon" + lon + "&appid=" + this.apiKey
-        )
-            .then((Response) => Response.json())
-    },
+  apiKey: "dba7d7b658e0ea9699b67a8ae8aabb6a",
+  weatherFetch: function (city) {
+    fetch(
+        "http://api.openweathermap.org/geo/1.0/direct?q="+ city +"&limit=5&appid="+this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data))
+      .catch(function (error) {
+        console.log("error", error);
+      });
+  },
 
-    displayWeather: function (data) {
-
-    }
-}
+  displayWeather: function (data) {
+    const lat = data[0].lat;
+    const lon = data[0].lon;
+    console.log(data);
+    return fetch(
+      "https://api.openweathermap.org/data/2.5/weather?lat=" +
+        lat +
+        "&lon=" +
+        lon +
+        "&appid=" +
+        this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+};
+weather.weatherFetch(city);
