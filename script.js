bade25bde628d8fa0b9d745d3d783b0fa7e1464d
MrTussa <79701873+MrTussa@users.noreply.github.com>
2023-02-25 20:37:10 +0500

    modified:   index.html modified:   script.js modified:   style.css modified:   style.css.map modified:   style.scss

--- script.js
+++ script.js
@@ -1,32 +1,68 @@
-const city = "London";
-
 let weather = {
   apiKey: "dba7d7b658e0ea9699b67a8ae8aabb6a",
-  weatherFetch: function (city) {
+  weatherGeo: function (city) {
     fetch(
-        "http://api.openweathermap.org/geo/1.0/direct?q="+ city +"&limit=5&appid="+this.apiKey
+      "http://api.openweathermap.org/geo/1.0/direct?q=" +
+        city +
+        "&limit=5&appid=" +
+        this.apiKey
     )
       .then((response) => response.json())
-      .then((data) => this.displayWeather(data))
+      .then((data) => this.weatherFetch(data))
       .catch(function (error) {
         console.log("error", error);
       });
   },
 
-  displayWeather: function (data) {
+  weatherFetch: function (data) {
     const lat = data[0].lat;
     const lon = data[0].lon;
-    console.log(data);
     return fetch(
       "https://api.openweathermap.org/data/2.5/weather?lat=" +
         lat +
         "&lon=" +
         lon +
         "&appid=" +
-        this.apiKey
+        this.apiKey +
+        "&units=metric"
     )
       .then((response) => response.json())
       .then((data) => this.displayWeather(data));
   },
+
+  displayWeather: function (data) {
+    const { name, weather, main, wind } = data;
+    const { icon, description } = weather[0];
+    const { temp, humidity } = main;
+    const { speed } = wind;
+    const cityElement = document.querySelector(".city");
+    const tempElement = document.querySelector(".temp");
+    const descElement = document.querySelector(".weather__desc");
+    const windElement = document.querySelector(".wind");
+    const humidityElement = document.querySelector(".weather__humidity");
+    const iconElement = document.querySelector(".weather__icon");
+    cityElement.innerText = "Weather in " + name;
+    tempElement.innerText = temp + "°C";
+    descElement.innerText = description;
+    windElement.innerText = "Wind speed: " + speed + "km/h";
+    humidityElement.innerText = "Humidity: " + humidity + "%";
+    iconElement.src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+  },
+  search: function () {
+    this.weatherGeo(document.querySelector(".search__bar").value);
+  },
 };
-weather.weatherFetch(city);
+
+document
+  .querySelector(".search__button")
+  .addEventListener("click", function () {
+    weather.search();
+  });
+
+document
+  .querySelector(".search__bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
